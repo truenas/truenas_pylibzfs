90c341cd5128c388b4d2b22d778f57e1cbeeb622
Andrew Walker <andrew.walker@truenas.com>
2025-11-14 08:30:31 -0600

    Handle broken ZFS error description in ZFSException
    If user attempts to zfs_open a path containing a multibyte
    character the libzfs error description gets munged due to
    %c which results in the ZFSException being turned into a
    simple error message due to PyUnicode_FromString failing
    to decode the munged character.

---
+++ tests/test_resource_invalid_multibyte.py
@@ -0,0 +1,28 @@
+# Check fix for NAS-138554
+
+import truenas_pylibzfs
+
+ERRDESC = 'Invalid multibyte character'
+
+
+def test_create_dataset_with_multibyte_name(root_dataset):
+    lz, root = root_dataset
+    rsrc_name = f'{root.name}/FOOBAR'
+
+    with pytest.raises(truenas_pylibzfs.ZFSException, match=ERRDESC) as exc:
+        lz.create_resource(
+            name=rsrc_name,
+            type=truenas_pylibzfs.ZFSType.ZFS_TYPE_FILESYSTEM,
+        )
+
+    assert exc.value.code == truenas_pylibzfs.ZFSError.EZFS_INVALIDNAME
+
+
+def test_open_dataset_with_multibyte_name(root_dataset):
+    lz, root = root_dataset
+    rsrc_name = f'{root.name}/FOOBAR'
+
+    with pytest.raises(truenas_pylibzfs.ZFSException, match=ERRDESC) as exc:
+        lz.open_resource(name=rsrc_name)
+
+    assert exc.value.code == truenas_pylibzfs.ZFSError.EZFS_INVALIDNAME
